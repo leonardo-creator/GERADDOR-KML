e0501671d90797ad78a473b32889a90e786ebe3a
leonardo-creator <LEONARDO.JUVENCIO018@gmail.com>
2025-05-30 19:48:55 -0300

    refactor: remove logo image from header in index.html

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,6 @@ export default function Header() {
     <header className="bg-white shadow-md py-4 px-6">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <div className="flex items-center">
-          <Image src="/brk.jpg" alt="Logo BRK" width={100} height={60} className="h-12 w-auto" />
         </div>
 
         <h1 className="text-center text-2xl md:text-3xl font-bold text-[#110043]">Visualizador e Gerador de KML</h1>
